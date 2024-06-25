a95e0a3f182be3704b03ab702fb69bc954ddefff
Ameba <uliasmorokova@gmail.com>
2024-06-25 15:36:56 +0300

    improved readability

--- src/main/main.go
+++ src/main/main.go
@@ -2,161 +2,45 @@ package main
 
 import (
 	"fmt"
+	calc "game/src/calculation"
+	objects "game/src/objects"
 	"image/color"
-	_ "image/jpeg"
-	_ "image/png"
-	"log"
-	"math/rand"
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/text"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 	"golang.org/x/image/font/basicfont"
 )
 
-const (
-	//Размер окна
-	screenHeight = 300
-	screenWidth  = 600
-	//Размер корабля
-	shipHeight = 25
-	shipWidth  = 25
-
-	//Размер пуль
-	bulletHeight = shipHeight / 5
-	bulletWidth  = shipWidth / 2
-
-	//Базовая скорость ускорения
-	shipAccelerationConstant = float64(0.0000000015)
-	//Множитель ускорения
-	shipAccelerationSpeedUpMultiplier = float64(2)
-	//Сопротивление, благодаря которому корабль замедляется при движении
-	shipResistence = float64(0.975)
-
-	//Размер и скорость метеоритов
-	meteorWidth  = 20
-	meteorHeight = 20
-	meteorSpeed  = float64(2.4)
-)
-
-var (
-	//Позиция корабля
-	shipPositionX = float64(screenWidth / 2)
-	shipPositionY = float64(screenHeight / 2)
-	//Движение корабля по оси X или Y
-	shipMovementX = float64(0)
-	shipMovementY = float64(0)
-
-	//Текущее ускорение корабля по оси X и Y
-	shipAccelerationX = float64(0)
-	shipAccelerationY = float64(0)
-
-	//Последнее обновление игры
-	prevUpdateTime = time.Now()
-
-	//Скорость пуль
-	bulletSpeedX = float64(3)
-
-	//Переменные, хранящие изображение для объектов
-	spaceImage     *ebiten.Image
-	spaceshipImage *ebiten.Image
-	meteorImage    *ebiten.Image
-)
-
-// Функция, загружающая файл с изображением корабля
-func init() {
-	var err error
-	spaceshipImage, _, err = ebitenutil.NewImageFromFile("textures/spaceship.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-// Функция, загружающая файл с изображением космоса
-func init() {
-	var err error
-	spaceImage, _, err = ebitenutil.NewImageFromFile("textures/space.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-// Функция, загружающая файл с изображением метеорита
-func init() {
-	var err error
-	meteorImage, _, err = ebitenutil.NewImageFromFile("textures/meteor.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-// Структура пуль (их позииция, скорость)
-type Bullet struct {
-	PositionX float64
-	PositionY float64
-	SpeedX    float64
-}
-
-// Структура метеоритов (их позиция, скорость)
-type Meteor struct {
-	PositionX float64
-	PositionY float64
-	SpeedX    float64
-}
-
 type Game struct {
-	//количество очков
-	score int
-	//смерть корабля (если значение == true - игра заканчивается)
-	shipDie     bool
-	pressedKeys []ebiten.Key
-	//фиксируем время, с которым появляется пуля, чтобы определенное количество пуль появлялось в свое время
-	//а не в разнобой
-	lastShotTime time.Time
-	//Точно такая же фиксация времени, с которым появляется враг
-	lastEnemyTime time.Time
-	//Список из количества существующих пуль
-	bullets []Bullet
-	//Список из количества существующиз метеоритов
-	meteorites []Meteor
+	pressedKey []ebiten.Key
+	score      int
+	Ship       calc.Ship
+	Meteor     calc.Meteor
+	Bullet     calc.Bullet
 }
 
-// Функция, создающая метеориты
-func NewMeteor() *Meteor {
-	return &Meteor{
-		//Метеориты появляется дальше видимого игрового поля
-		//чтобы у игрока было время на подготовку к игре
-		PositionX: screenWidth + 300,
-		PositionY: float64(rand.Intn(screenHeight - meteorHeight)),
-		SpeedX:    -meteorSpeed,
-	}
+func (g *Game) Layout(outsideWitdh, outsideHeight int) (int, int) {
+	return calc.ScreenWidth, calc.ScreenHeight
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return screenWidth, screenHeight
-}
-
-// Функция, при которой метеорит, в которого попадает пуля, удалется
+// Проверка координат пуль и координат метеоритов
 func (g *Game) checkCollision() {
-	for i := 0; i < len(g.bullets); i++ {
-		bullet := g.bullets[i]
-		for j := 0; j < len(g.meteorites); j++ {
-			meteor := g.meteorites[j]
-			//Проверяем, не совпадают ли координаты пулиь и метеорита
-			//только в том случае, если пуля находится в пределах игрового поля
-			//чтобы игрок не мог разрушать метеориты еще до того, как они появятся на окне
-			if bullet.PositionX <= screenWidth {
-				if bullet.PositionX < meteor.PositionX+meteorWidth &&
-					bullet.PositionX+bulletWidth > meteor.PositionX &&
-					bullet.PositionY < meteor.PositionY+meteorHeight &&
-					bullet.PositionY+bulletHeight > meteor.PositionY {
-					//Если координаты совпали, то мы удаляем и пулю, и метеорит, в который попала пуля, из списка
-					//Чтобы они исчезли с игровго экрана
-					g.meteorites = append(g.meteorites[:j], g.meteorites[j+1:]...)
-					g.bullets = append(g.bullets[:i], g.bullets[i+1:]...)
+	for i := 0; i < len(g.Bullet.Bullets); i++ {
+		bullet := g.Bullet.Bullets[i]
+		for j := 0; j < len(g.Meteor.Meteorites); j++ {
+			meteor := g.Meteor.Meteorites[j]
+			// Игрок может попасть в метеорит только в том случае, если метеорит появляется в "поле зрения"
+			if bullet.Position.X <= calc.ScreenWidth {
+				if bullet.Position.X < meteor.Position.X+calc.MeteorWidth &&
+					bullet.Position.X+calc.BulletWidth > meteor.Position.X &&
+					bullet.Position.Y < meteor.Position.Y+calc.MeteorHeight &&
+					bullet.Position.Y+calc.BulletHeight > meteor.Position.Y {
+					// При попадании пули в метеорит, пуля и метеорит удаляется
+					g.Meteor.Meteorites = append(g.Meteor.Meteorites[:j], g.Meteor.Meteorites[j+1:]...)
+					g.Bullet.Bullets = append(g.Bullet.Bullets[:i], g.Bullet.Bullets[i+1:]...)
 					g.score++
 					i--
 					break
@@ -168,15 +52,15 @@ func (g *Game) checkCollision() {
 
 // Функция проигрыша
 func (g *Game) gameOver() bool {
-	for i := 0; i < len(g.meteorites); i++ {
-		enemy := g.meteorites[i]
+	for i := 0; i < len(g.Meteor.Meteorites); i++ {
+		enemy := g.Meteor.Meteorites[i]
 		//Проверяем, совпадают ли координаты корабля, с координатами врага
-		if shipPositionX < enemy.PositionX+meteorWidth &&
-			shipPositionX+shipWidth > enemy.PositionX &&
-			shipPositionY+shipHeight > enemy.PositionY &&
-			shipPositionY < enemy.PositionY+meteorHeight {
+		if calc.ShipPositionX < enemy.Position.X+calc.MeteorWidth &&
+			calc.ShipPositionX+calc.ShipWidth > enemy.Position.X &&
+			calc.ShipPositionY+calc.ShipHeight > enemy.Position.Y &&
+			calc.ShipPositionY < enemy.Position.Y+calc.MeteorHeight {
 			//Если координаты совпадают, поле shipDie в структуре Game изменяется на true, чтобы игра остановилась
-			g.shipDie = true
+			g.Ship.ShipDie = true
 			return true
 		}
 	}
@@ -184,195 +68,112 @@ func (g *Game) gameOver() bool {
 }
 
 // Функция перезапуска игры
-func (g *Game) restart() {
+func (g *Game) Restart() {
 	//Очищаем списки с метеоритами и пулями
 	//чтобы начать игровой процесс заново
-	g.bullets = g.bullets[:0]
-	g.meteorites = g.meteorites[:0]
+	g.Bullet.Bullets = g.Bullet.Bullets[:0]
+	g.Meteor.Meteorites = g.Meteor.Meteorites[:0]
 	//Обнуляем счетик очков
 	g.score = 0
 	//"Воскрешаем" корабль, чтобы игра снова заработала
-	g.shipDie = false
+	g.Ship.ShipDie = false
 	//Возвращаем корабль на его позицию
-	shipPositionX = float64(screenWidth / 2)
-	shipPositionY = float64(screenHeight / 2)
+	calc.ShipPositionX = float64(calc.ScreenWidth / 2)
+	calc.ShipPositionY = float64(calc.ScreenHeight / 2)
 	//Заново создаем метеориты
-	for i := range g.meteorites {
-		g.meteorites[i] = *NewMeteor()
+	for i := range g.Meteor.Meteorites {
+		g.Meteor.Meteorites[i] = *calc.NewMeteor()
 	}
 	//Возобновляем скорость пуль
-	for i := range g.bullets {
-		g.bullets[i].SpeedX = bulletSpeedX
+	for i := range g.Bullet.Bullets {
+		g.Bullet.Bullets[i].Speed = g.Bullet.Speed
 	}
 
 }
 
 func (g *Game) Update() error {
-	//Дельта времени последнего обновления
-	timeDelta := float64(time.Since(prevUpdateTime))
-	prevUpdateTime = time.Now()
-
-	//Определяем, какие клавиши нажимает игрок
-	g.pressedKeys = inpututil.AppendPressedKeys(g.pressedKeys[:0])
-	shipAccelerationX = 0
-	shipAccelerationY = 0
-
-	//Базовая скорость корабля
-	acc := shipAccelerationConstant
-
-	//Уравление кораблём
-	for _, key := range g.pressedKeys {
+	pressedKeys := inpututil.AppendPressedKeys(g.pressedKey)
+	for _, key := range pressedKeys {
 		switch key.String() {
-		//Если игрок нажимает пробел, то корабль ускоряется
-		case "Space":
-			acc *= shipAccelerationSpeedUpMultiplier
-		}
-	}
-	for _, key := range g.pressedKeys {
-		switch key.String() {
-		//Если игрок нажимает клавишу стрелки вверх, то и корабль соответственно поднимается
-		case "ArrowDown":
-			shipAccelerationY = acc
-		//"Стрелка вниз"-корабль летит вниз
-		case "ArrowUp":
-			shipAccelerationY = -acc
-		//"Стрелка вправо"-корабль летит вправо
-		case "ArrowRight":
-			shipAccelerationX = acc
-		//"Стрелка влево"-корабль летит влево
-		case "ArrowLeft":
-			shipAccelerationX = -acc
-		//Клавиша "R" отвечает за рестарт игры. Но она работает только в том случае, если игрок проиграл
 		case "R":
-			if g.shipDie {
-				g.restart()
+			if g.Ship.ShipDie {
+				g.Restart()
 			}
 		}
+
 	}
-	//Если игрок проиграл, изменяем состояние корабля
+
+	// Обновление состояния корабля
+	g.Ship.ShipUpdate(calc.ShipWidth, calc.ShipHeight)
+
+	// При проигрыше все объекты останавливаются
 	if g.gameOver() {
-		g.shipDie = true
+		g.Ship.ShipDie = true
+		g.Meteor.Dead = true
+		g.Bullet.Dead = true
 	}
 
-	//Если корабль мертв, обнуляем скорость у всех объектов и не даем кораблю двигаться, чтобы вся игра замерла
-	if g.shipDie {
-		shipMovementX = 0
-		shipMovementY = 0
-		shipAccelerationX = 0
-		shipAccelerationY = 0
-		for i := range g.meteorites {
-			g.meteorites[i].SpeedX = 0
-		}
-		for i := range g.bullets {
-			g.bullets[i].SpeedX = 0
-		}
+	// Если корабль "мертв", то и метеориты и пули замирают
+	if g.Ship.ShipDie {
+		g.Meteor.MeteorUpdate()
+		g.Bullet.BulletUpdate()
+
 	}
 
-	//Скорость движения по оси X и Y
-	shipMovementY += shipAccelerationY
-	shipMovementX += shipAccelerationX
-	shipMovementX *= shipResistence
-	shipMovementY *= shipResistence
-	shipPositionX += shipMovementX * timeDelta
-	shipPositionY += shipMovementY * timeDelta
-
-	//Константы с минимальным значением координат X и Y (соответственно - 0)
-	//и с максимальным значением (конец игрового поля)
-	const minX = 0
-	const minY = 0
-	const maxX = screenWidth - shipWidth
-	const maxY = screenHeight - shipHeight
-
-	//Если корабль "соприкосается" с границами окна, то дальше он пройти не сможет
-	if shipPositionX >= maxX || shipPositionX <= minX {
-		if shipPositionX > maxX {
-			shipPositionX = maxX
-		} else if shipPositionX < minX {
-			shipPositionX = minX
-		}
-		shipMovementX *= -1
+	// Обновление состояния метеоритов
+	for time.Since(g.Meteor.LastMeteor) > time.Second*1 {
+		g.Meteor.Meteorites = append(g.Meteor.Meteorites, *calc.NewMeteor())
+		g.Meteor.LastMeteor = time.Now()
 	}
 
-	if shipPositionY >= maxY || shipPositionY <= minY {
-		if shipPositionY > maxY {
-			shipPositionY = maxY
-		} else if shipPositionY < minY {
-			shipPositionY = minY
-		}
-		shipMovementY *= -1
+	for v := range g.Meteor.Meteorites {
+		g.Meteor.Meteorites[v].Position.X += g.Meteor.Meteorites[v].Speed.X
 	}
 
-	//Создаем пули
-	if time.Since(g.lastShotTime) > time.Millisecond*500 {
-		g.bullets = append(g.bullets, Bullet{
-			PositionX: shipPositionX,
-			PositionY: shipPositionY + 10,
-			SpeedX:    bulletSpeedX,
-		})
-
-		g.lastShotTime = time.Now()
+	// Обновление состояния пуль
+	if time.Since(g.Bullet.LastShotTime) > time.Millisecond*500 {
+		g.Bullet.Bullets = append(g.Bullet.Bullets, *calc.NewBullet())
+		g.Bullet.LastShotTime = time.Now()
 	}
 
-	for i := range g.bullets {
-		g.bullets[i].PositionX += g.bullets[i].SpeedX
+	for i := range g.Bullet.Bullets {
+		g.Bullet.Bullets[i].Position.X += g.Bullet.Bullets[i].Speed
 	}
 
-	//Создаем врагов
-	for time.Since(g.lastEnemyTime) > time.Second*1 {
-		g.meteorites = append(g.meteorites, *NewMeteor())
-		g.lastEnemyTime = time.Now()
-	}
-
-	//Позиция метеоритов по оси меняется в зависимости от их скорости
-	for v := range g.meteorites {
-		g.meteorites[v].PositionX += g.meteorites[v].SpeedX
-	}
-
-	//Проверяем, попала ли пуля в метеорит
 	g.checkCollision()
-
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	//Прикрепляем фоновое изображение с космосом
-	screen.DrawImage(spaceImage, nil)
-	//Ставим изображение корабля под его координаты
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(shipPositionX-15, shipPositionY-12)
-	//Рисуем пули с помощью функции из библиотеки
-	for _, bullet := range g.bullets {
-		vector.DrawFilledRect(screen, float32(bullet.PositionX), float32(bullet.PositionY), float32(bulletWidth), float32(bulletHeight), color.RGBA{255, 0, 0, 255}, false)
-	}
-	//Рисуем корабль поверх пуль
-	screen.DrawImage(spaceshipImage, op)
+	// отрисовка фона
+	spaceInstance := &objects.Objects{}
+	spaceInstance.DrawBackground(screen)
 
-	//Рисуем метеориты
-	for _, meteor := range g.meteorites {
-		opt := &ebiten.DrawImageOptions{}
-		opt.GeoM.Translate(meteor.PositionX-5, meteor.PositionY-5)
-		screen.DrawImage(meteorImage, opt)
+	for _, bullet := range g.Bullet.Bullets {
+		vector.DrawFilledRect(screen, float32(bullet.Position.X), float32(bullet.Position.Y), float32(calc.BulletWidth), float32(calc.BulletHeight), color.RGBA{255, 0, 0, 255}, false)
+	}
+	// отрисовка корабля
+	spaceInstance.DrawSpaceShip(screen, float64(calc.ShipPositionX), float64(calc.ShipPositionY))
+
+	// отрисовка метеоритов
+
+	for _, meteor := range g.Meteor.Meteorites {
+		spaceInstance.DrawMeteorit(screen, meteor.Position.X, meteor.Position.Y)
 	}
 
-	//определяем шрифт
+	// счетчик количества очков
 	face := basicfont.Face7x13
-
-	//При проигрыше появляется соответствующая надпись
-	if g.shipDie {
-		text.Draw(screen, "Game Over", face, screenWidth/2-40, screenHeight/2, color.White)
-		text.Draw(screen, "Press 'R' to restart", face, screenWidth/2-60, screenHeight/2+16, color.White)
+	if g.Ship.ShipDie {
+		text.Draw(screen, "Game Over", face, calc.ScreenWidth/2-40, calc.ScreenHeight/2, color.White)
+		text.Draw(screen, "Press 'R' to restart", face, calc.ScreenWidth/2-60, calc.ScreenHeight/2+16, color.White)
 	}
-
-	//Выводим счетчик очков на кэкран
 	scoreText := fmt.Sprintf("Score: %d", g.score)
-	text.Draw(screen, scoreText, face, 5, screenHeight-5, color.White)
+	text.Draw(screen, scoreText, face, 5, calc.ScreenHeight-5, color.White)
 }
 
 func main() {
-	//Создаем окно
-	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+	ebiten.SetWindowSize(calc.ScreenWidth*2, calc.ScreenHeight*2)
 	ebiten.SetWindowTitle("Spaceship game")
-
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		panic(err)
 	}
